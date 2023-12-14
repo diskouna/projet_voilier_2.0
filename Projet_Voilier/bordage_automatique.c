41c61956827268cfccf3e4f883f0b6755f88d0f0
sandruxflux <139953457+sandruxflux@users.noreply.github.com>
2023-12-14 20:13:58 +0100

    Update bordage_automatique.c

--- Projet_Voilier/bordage_automatique.c
+++ Projet_Voilier/bordage_automatique.c
@@ -7,12 +7,12 @@
 
 float bordage(float angle_alpha) {
 	float angle_deg;
-
+// Si l'angle du vent arrive entre -45 et +45 par rapport à l'axe du bateau, 
 	if (angle_alpha>=45 && angle_alpha<=180 ) {
-		angle_deg = ((angle_alpha-45)*90)/135;}
+		angle_deg = ((angle_alpha-45)*90)/135;} // On implémente une fonction linéaire qui va de 0 à 90 selon le vent
 	else{
-		angle_deg = 0;}
-	MyServo_Rotate(angle_deg);
+		angle_deg = 0;} // Sinon, on se remet dans l'axe
+	MyServo_Rotate(angle_deg); // Le servomoteur recupère l'angle affecté et s'assure que le voilier se place correctement
 		
 		return angle_deg;
 }
